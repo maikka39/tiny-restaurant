05fc2c116f007280668d51f1e5da6a2b9d1d8d31
matsn0w <bartscholtus@gmail.com>
2021-03-03 11:23:38 +0100

    basic twill setup

--- config/twill.php
+++ config/twill.php
@@ -1,4 +1,21 @@
 <?php
 
 return [
+    'enabled' => [
+        'users-management' => true,
+        'media-library' => true,
+        'file-library' => true,
+        'block-editor' => true,
+        'buckets' => true,
+        'users-image' => true,
+        'settings' => true,
+        'dashboard' => true,
+        'search' => true,
+        'users-description' => false,
+        'activitylog' => true,
+        'users-2fa' => true,
+        'users-oauth' => false,
+    ],
+    'locale' => 'nl',
+    'fallback_locale' => 'en',
 ];
