ffc415cc783ecfa0e8b8bbba8f6769fdd165c377
matsn0w <bartscholtus@gmail.com>
2021-03-10 12:43:19 +0100

    create and view static pages

---
+++ resources/views/admin/pages/form.blade.php
@@ -0,0 +1,12 @@
+@extends('twill::layouts.form')
+
+@section('contentFields')
+    @formField('wysiwyg', [
+        'name' => 'description',
+        'label' => 'Description'
+    ])
+
+    @formField('block_editor', [
+        'blocks' => ['text']
+    ])
+@stop
---
+++ resources/views/site/pages/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+
+@section('content')    
+    <h1>{{ $item->title }}</h1>
+    {!!  $item->description !!}
+    {!! $item->renderBlocks(false) !!}
+@endsection
