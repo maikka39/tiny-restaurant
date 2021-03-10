b07195d860bef4b619b0e1bd7af6d38b64c3abd1
MaxvdVossenberg <62118062+MaxvdVossenberg@users.noreply.github.com>
2021-03-10 15:08:10 +0100

    Added more logo's to logo selectbox
    Added some of the last styling to make the logo's a little bit bigger.
    Also added common logo's to the selectbox for the logo selector

--- resources/views/admin/blocks/social_media_links_item.blade.php
+++ resources/views/admin/blocks/social_media_links_item.blade.php
@@ -26,6 +26,34 @@
         [
             'value' => 'fas fa-phone',
             'label' => 'Telefoon logo'
+        ],
+        [
+            'value' => 'fab fa-instagram',
+            'label' => 'Instagram logo'
+        ],
+        [
+            'value' => 'fab fa-snapchat',
+            'label' => 'Snapchat logo'
+        ],
+        [
+            'value' => 'fab fa-linkedin',
+            'label' => 'LinkedIn logo'
+        ],
+        [
+            'value' => 'fab fa-twitch',
+            'label' => 'Twitch logo'
+        ],
+        [
+            'value' => 'fab fa-youtube',
+            'label' => 'Youtube logo'
+        ],
+        [
+            'value' => 'fab fa-facebook-messenger',
+            'label' => 'Facebook Messenger logo'
+        ],
+        [
+            'value' => 'fab fa-whatsapp',
+            'label' => 'Whatsapp logo'
         ]
     ]
 ])
--- resources/views/site/blocks/social_media_links.blade.php
+++ resources/views/site/blocks/social_media_links.blade.php
@@ -1,8 +1,8 @@
-<div class="flex justify-between items-center">
+<div class="flex justify-between items-center text-lg">
     @foreach($block->children as $child)
         <a class="px-4 flex-col justify-center" href="{{ $child->input('link') }}" style="color: {{ $child->input('logo_color') }}"> <!-- phone number of Krista -->
             <i class="{{ $child->input('logo') }}"></i>
-            <span class="sr-only">Telefoon</span> {{-- TODO future translation --}}
+            <span class="sr-only">{{ $child->input('logo') }}</span>
         </a>
     @endforeach
 </div>
