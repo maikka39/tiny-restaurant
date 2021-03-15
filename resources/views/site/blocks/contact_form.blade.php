d0c63be835deeb100e42ffd245f6cbd647c194bd
Bryan de Jong <bryandejong99@gmail.com>
2021-03-15 01:58:35 +0100

    Add contact form styling

--- resources/views/site/blocks/contact_form.blade.php
+++ resources/views/site/blocks/contact_form.blade.php
@@ -1,31 +1,29 @@
-<div>
-    <h1>{{ $block->input('title') }}</h1>
-    <p>{{ $block->input('description') }}</p>
-    <form method="POST" action="/contact">
+<div class="p-2 mx-2">
+    <h1 class="text-4xl">{{ $block->input('title') }}</h1>
+    <p class="my-2">{{ $block->input('description') }}</p>
+    <form class="p-2 pl-4 bg-gray-200" method="POST" action="/contact">
         @csrf
-        <div>
-            <label for="name">
+        <div class="pb-2 lg">
+            <label class="text-xl block" for="name">
                 Naam
             </label>
-            <input id="name" type="text" placeholder="Uw naam">
+            <input class="p-1 rounded w-full block border border-gray-500" id="name" type="text" placeholder="Uw naam">
         </div>
-        <div>
-            <label for="email">
+        <div class="py-2 block">
+            <label class="text-xl block" for="email">
                 E-mail
             </label>
-            <input id="email" type="email" placeholder="voorbeeld@email.com">
+            <input class="p-1 rounded block w-full border  border-gray-500" id="email" type="email" placeholder="voorbeeld@email.com">
         </div>
-        <div>
-            <label for="message">
-                Message
+        <div class="py-2">
+            <label class="text-xl block" for="message">
+                Bericht
             </label>
-            <textarea
-                    id="message"></textarea>
+            <textarea class="p-1 rounded w-full block border  border-gray-500" id="message"></textarea>
         </div>
-        <div>
-            <button
-                    type="button">
-                Send
+        <div class="py-3">
+            <button class="px-2 rounded text-lg text-white border border-green-900 bg-green-700" type="button">
+                Vestuur
             </button>
         </div>
     </form>
