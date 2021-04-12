025c7ab86d78c7c9fba3235271d2784d785227f6
matsn0w <bartscholtus@gmail.com>
2021-04-12 14:57:57 +0200

    typograhpy

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,4 +3,15 @@
 @section('content')
     <a href="#" class="button primary">Primaire knop</a>
     <a href="#" class="button secondary">Secundaire knop</a>
+    <a href="#" class="button">Secundaire knop</a>
+    <button href="#" class="button primary">Secundaire knop</button>
+
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore ipsam saepe doloribus minus quod asperiores eligendi? Praesentium, in mollitia!</p>
 @endsection
