6a24c4b64423ea59fee58238ddbf61a95ab35400
Bryan de Jong <bryandejong99@gmail.com>
2021-04-14 22:53:47 +0200

    Add required form fields to homepage settings

--- resources/views/admin/settings/homepage.blade.php
+++ resources/views/admin/settings/homepage.blade.php
@@ -3,14 +3,34 @@
 
 @section('contentFields')
     @formField('medias', [
-    'name' => 'image',
-    'label' => 'Banner afbeelding'
+        'name' => 'banner_image',
+        'label' => 'Banner afbeelding'
     ])
 
     @formField('input', [
-    'name' => 'description',
-    'type' => 'text',
-    'label' => 'Banner text',
-    'maxlength' => 50
+        'name' => 'banner_title',
+        'type' => 'text',
+        'label' => 'Banner title',
+        'maxlength' => 50
     ])
+
+    @formField('input', [
+        'name' => 'banner_description',
+        'type' => 'text',
+        'label' => 'Banner description',
+        'maxlength' => 50
+    ])
+
+    @formField('wysiwyg', [
+        'name' => 'body_text',
+        'label' => 'Body tekst',
+        'placeholder' => 'Vul een tekst in',
+        'maxlength' => 2500
+    ])
+
+    @formField('medias', [
+        'name' => 'body_image',
+        'label' => 'Body afbeelding'
+    ])
+
 @stop
