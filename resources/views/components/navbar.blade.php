85229cec332f1c837988e83cb3e63de65ebf2bf0
xpetersen <ptrsnxndr@gmail.com>
2021-04-15 19:44:04 +0200

    removed unused divs

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -33,9 +33,9 @@
     <div class="sm:hidden" id="mobile-menu">
         <div class="mobile-navbar">
             <div class="mobile-navbar-flex" id="mobileMenu">
-                <div><a href="{{url('/')}}" class="navbar-text navbar-text-small" aria-current="page">Home</a></div>
-                <div><a href="{{url('/nieuws')}}" class="navbar-text navbar-text-small">Nieuws</a></div>
-                <div><a href="{{url('#')}}" class="navbar-text navbar-text-small">Agenda</a></div>
+                <a href="{{url('/')}}" class="navbar-text navbar-text-small" aria-current="page">Home</a>
+                <a href="{{url('/nieuws')}}" class="navbar-text navbar-text-small">Nieuws</a>
+                <a href="{{url('#')}}" class="navbar-text navbar-text-small">Agenda</a>
                 <div class="dropdown">
                     <button class="dropdown-button navbar-text navbar-text-small w-auto">Gemeentes</button>
                     <div class="dropdown-content">
@@ -44,7 +44,7 @@
                         @endforeach
                     </div>
                 </div>
-                <div><a href="{{url('/contact')}}" class="navbar-text navbar-text-small">Contact</a></div>
+                <a href="{{url('/contact')}}" class="navbar-text navbar-text-small">Contact</a>
             </div>
         </div>
     </div>
