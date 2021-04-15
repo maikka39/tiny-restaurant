de96cd99fb223b80374fe2a0ac833edf6b42ce39
Bryan de Jong <bryandejong99@gmail.com>
2021-04-15 11:15:12 +0200

    Remove test data

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -15,19 +15,16 @@
         <div class="grid grid-cols-3 py-1">
             <div class="col-span-2">
                 <h2>Activiteiten</h2>
+                <p>Momenteel geen activiteiten</p>
             </div>
             <div class="col-span-1 mt-48 grid gap-y-6 md:gap-y-12">
                 <div class="sidecard row">
                     <h4>Agenda</h4>
-                    <span>11/2 Proeverij Eindhoven</span>
-                    <span>17/2 Proeverij Laarbeek</span>
-                    <span>10/3 Proeverij Helmond</span>
+                    <p>Agenda is leeg</p>
                 </div>
                 <div class="sidecard row">
                     <h4>Links</h4>
-                    <a>https://www.groentjessoep.nl/</a>
-                    <a>https://www.smaakcentrum.nl/</a>
-                    <a>https://www.jonglereneten.nl/</a>
+                    <p>Geen links</p>
                 </div>
             </div>
         </div>
