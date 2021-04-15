90d2909f867d6debfb119d6e478b6d41835b3a58
Bryan de Jong <bryandejong99@gmail.com>
2021-04-15 11:50:45 +0200

    Remove banner block

--- resources/views/site/blocks/banner.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="relative flex">
-    <img class="flex flex-grow" src="{{ $block->image('image', 'desktop') }}" alt="{{ $block->imageAltText('image') }}">
-
-    @if($block->input('description'))
-        <div class="bg-white p-1 absolute top-1/2 right-1/3">
-            {{ $block->input('description') }}
-        </div>
-    @endif
-</div>
