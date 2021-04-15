822db4d5827488d0c7e7ce8cb1e1f241d8d1f0a8
Maik de Kruif <maik@maik.dev>
2021-04-15 19:43:50 +0200

    Add slideshow (no css or js yet..)

--- resources/views/site/blocks/partners.blade.php
+++ resources/views/site/blocks/partners.blade.php
@@ -1,7 +1,39 @@
-<div class="flex justify-between items-center text-lg flex-col">
+@push('styles')
+    <link href="{{ asset('css/slideshow.css') }}" rel="stylesheet">
+@endpush
+@push('scripts')
+    <script type="text/javascript" src="{{asset('js/slideshow.js')}}" defer></script>
+@endpush
+
+<div class="flex-container">
+    <div class="slideshow-container">
+        @foreach($block->children as $child)
+            {{-- <h1>{{$child->input('name')}}</h1>
+            <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}">
+            <p>{!!$child->input('description')!!}</p> --}}
+
+            <div class='mySlides'>
+                <div class="imgholder">
+                    <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}">
+                </div>
+                <div class='slidetextbox'>
+                    <h4>{{$child->input('name')}}</h4>
+                    <p>{!!$child->input('description')!!}</p>
+                    {{-- <a href='{{ route('farmer.show', $farmer->slug) }}' class='button primary '> Lees meer </a> --}}
+                </div>
+            </div>
+        @endforeach
+
+        <a class="prev">&#10094;</a>
+        <a class="next">&#10095;</a>
+    </div>
+    <br>
+
+    <div id='slideshowdots'>
+    </div>
+</div>
+<div class="flex flex-row flex-wrap justify-around content-around">
     @foreach($block->children as $child)
-        <h1>{{$child->input('name')}}</h1>
-        <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}">
-        <p>{!!$child->input('description')!!}</p>
+        <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}" class="w-1/5 rounded-3xl m-4">
     @endforeach
 </div>
