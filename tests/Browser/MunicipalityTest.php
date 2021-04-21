d40f08763bdb5cae9ef32e6456115903339d9d24
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-04-21 16:39:59 +0200

    Revert "Reverted twill:update back to twill:build to check if the test will run succesfully"
    This reverts commit aa6b55b7254d7cf59680a70d24b0ea531ebf237f.

--- tests/Browser/MunicipalityTest.php
+++ tests/Browser/MunicipalityTest.php
@@ -14,7 +14,7 @@ class MunicipalityTest extends DuskTestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->artisan('twill:build');
+        $this->artisan('twill:update');
         $this->artisan('db:seed --class=SuperAdminSeeder');
     }
 
