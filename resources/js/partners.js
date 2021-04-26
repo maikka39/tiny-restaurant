4fe02a09145e10b7349058145d4fab0ec552cdce
Maik de Kruif <maik@maik.dev>
2021-04-26 16:03:28 +0200

    Remove debug code

--- resources/js/partners.js
+++ resources/js/partners.js
@@ -1,7 +1,6 @@
 require("./slideshow")
 
 window.togglePartnerPopup = (el) => {
-  console.log(el)
   if (el.style.display === "block") {
     el.style.display = "none"
     document.body.classList.remove("no-scroll")
