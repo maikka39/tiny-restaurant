5253f807801b87674eaa0db25696158f1b5c1db0
Maik de Kruif <maik@maik.dev>
2021-04-26 15:50:19 +0200

    Add a header

--- resources/views/site/blocks/partners.blade.php
+++ resources/views/site/blocks/partners.blade.php
@@ -5,35 +5,38 @@
     <script type="text/javascript" src="{{asset('js/partners.js')}}"></script>
 @endpush
 
-<div class="flex-container slideshow">
-    <div class="slideshow-container">
-        @foreach($block->children->shuffle() as $child)
-            <div class='mySlides'>
-                <div class="imgholder">
-                    <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}">
+<div class="partners">
+    <h2>Partners</h2>
+    <div class="flex-container slideshow">
+        <div class="slideshow-container">
+            @foreach($block->children->shuffle() as $child)
+                <div class='mySlides'>
+                    <div class="imgholder">
+                        <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}">
+                    </div>
+                </div>
+            @endforeach
+
+            <a class="prev">&#10094;</a>
+            <a class="next">&#10095;</a>
+        </div>
+        <br>
+
+        <div class="slideshowdots">
+        </div>
+    </div>
+    <div class="flex flex-row flex-wrap justify-around content-around partner-list">
+        @foreach($block->children as $child)
+            <div class="form-popup" onclick="event.target === this ? togglePartnerPopup(this) : console.log(1)">
+                <div class="modal">
+                    <h2>{{$child->input('name')}}</h2>
+                    <p>{!!$child->input('description')!!}</p>
+                    <button class="close" href="#" onclick="togglePartnerPopup(this.parentElement.parentElement)">X</button>
                 </div>
             </div>
-        @endforeach
-
-        <a class="prev">&#10094;</a>
-        <a class="next">&#10095;</a>
-    </div>
-    <br>
-
-    <div class="slideshowdots">
-    </div>
-</div>
-<div class="flex flex-row flex-wrap justify-around content-around partner-list">
-    @foreach($block->children as $child)
-        <div class="form-popup" onclick="event.target === this ? togglePartnerPopup(this) : console.log(1)">
-            <div class="modal">
-                <h2>{{$child->input('name')}}</h2>
-                <p>{!!$child->input('description')!!}</p>
-                <button class="close" href="#" onclick="togglePartnerPopup(this.parentElement.parentElement)">X</button>
+            <div class="w-1/5 m-4">
+                <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}" class="rounded-3xl cursor-pointer" onclick="togglePartnerPopup(this.parentElement.parentElement.getElementsByClassName('form-popup')[0])">
             </div>
-        </div>
-        <div class="w-1/5 m-4">
-            <img src="{{$child->image('image', 'desktop')}}" alt="{{$child->imageAltText('image')}}" class="rounded-3xl cursor-pointer" onclick="togglePartnerPopup(this.parentElement.parentElement.getElementsByClassName('form-popup')[0])">
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
