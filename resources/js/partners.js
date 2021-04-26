371d92ff61153e8ff19c2b6f974af9194c528133
Maik de Kruif <maik@maik.dev>
2021-04-26 15:56:43 +0200

    Use 2 spaces for consistency

--- resources/js/partners.js
+++ resources/js/partners.js
@@ -1,11 +1,12 @@
 require("./slideshow")
 
 window.togglePartnerPopup = (el) => {
-    if (el.style.display === "block") {
-        el.style.display = "none"
-        document.body.classList.remove("no-scroll")
-    } else {
-        el.style.display = "block"
-        document.body.classList.add("no-scroll")
-    }
+  console.log(el)
+  if (el.style.display === "block") {
+    el.style.display = "none"
+    document.body.classList.remove("no-scroll")
+  } else {
+    el.style.display = "block"
+    document.body.classList.add("no-scroll")
+  }
 }
