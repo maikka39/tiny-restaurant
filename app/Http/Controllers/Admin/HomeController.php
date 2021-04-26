89fc862365e456ba58dc8c785b30fce448d7ffec
Niels van Hal <ntj.vanhal@student.avans.nl>
2021-04-26 09:40:56 +0200

    layout free edits

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Repositories\HomeRepository;
-use Illuminate\Routing\Controller;
+use App\Models\Home;
 use App\Models\Homepage;
+use Illuminate\Routing\Controller;
+use App\Repositories\HomeRepository;
 use App\Http\Requests\HomeSettingsRequest;
 
 class HomeController extends Controller
@@ -13,12 +14,19 @@ class HomeController extends Controller
     //Extra 'concept setting pages' could be made using other id's
     public function show() 
     {
-        return view('admin.homesettings.form');
+        $homepage = Home::find(1);
+        $bannerTitle = $homepage->settings->where('key', 'banner_title')->first()->value ?? "Het Tiny Restaurant";
+        $bannerDescription = $homepage->settings->where('key', 'banner_description')->first()->value ?? "Wilt u als bedrijf ook het verschil maken en waardevol ondernemen door naar uw doelgroep toe te gaan?";
+
+        return view('admin.homesettings.form', [
+            'bannerTitle' => $bannerTitle,
+            'bannerDescription' => $bannerDescription
+        ]);
     }
 
     public function view()
     {
-        $homepage = Homepage::find(1);
+        $homepage = Home::find(1);
         $bannerTitle = $homepage->settings->where('key', 'banner_title')->first()->value ?? "Het Tiny Restaurant";
         $bannerDescription = $homepage->settings->where('key', 'banner_description')->first()->value ?? "Wilt u als bedrijf ook het verschil maken en waardevol ondernemen door naar uw doelgroep toe te gaan?";
     
--- resources/views/admin/homesettings/form.blade.php
+++ resources/views/admin/homesettings/form.blade.php
@@ -5,8 +5,8 @@
     @csrf
 
   <a17-fieldset>
-    <a17-textfield name="banner_title" label="Banner Title"></a17-textfield>
-    <a17-textfield name="banner_description" label="Banner Beschrijving"></a17-textfield>
+    <a17-textfield :maxlength="50" name="banner_title" label="Banner Title" :initial-value="'{{$bannerTitle}}'"></a17-textfield>
+    <a17-textfield :maxlength="150" name="banner_description" label="Banner Beschrijving" :initial-value="'{{$bannerDescription}}'"></a17-textfield>
   </a17-fieldset>
   <a17-button variant="validate" type="submit">Update</a17-button>
 </form>
