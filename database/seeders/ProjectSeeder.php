3c1fe3f064ccc7d3c196b528469a41e19a0756a7
Bram van den Boomen <b.vandenboomen@student.avans.nl>
2021-04-30 08:42:58 +0200

    The municipality page now contains projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Municipality;
 use App\Models\Slugs\ProjectSlug;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -22,35 +23,48 @@ class ProjectSeeder extends Seeder
                 'active' => '0',
                 'description' => '<p>Welkom in de Blackboard course voor SOPRJ8. Je bent automatisch geenrolled en toegevoegd aan je projectgroep zoals deze in de SOPRJ7 blackboard course zat. Mocht dit onjuist zijn, laat dit dan even aan mij (Rik Meijer) even weten.</p><p>Verder wensen wij je weer veel leerzaam plezier komende weken!</p>',
                 'date' => '2021-04-15 12:00:00',
-                "address" => '1111AA'
+                "address" => '1111AA',
+                "municipality_id" => '1'
             ],[
                 'published' => '1',
                 'name' => 'Project B',
                 'active' => '0',
                 'description' => '<p>Welkom in de Blackboard course voor SOPRJ8. Je bent automatisch geenrolled en toegevoegd aan je projectgroep zoals deze in de SOPRJ7 blackboard course zat. Mocht dit onjuist zijn, laat dit dan even aan mij (Rik Meijer) even weten.</p><p>Verder wensen wij je weer veel leerzaam plezier komende weken!</p>',
                 'date' => '2021-04-15 12:00:00',
-                "address" => '1111AA'
+                "address" => '1111AA',
+                "municipality_id" => '1'
             ],[
                 'published' => '1',
                 'name' => 'Project C',
                 'active' => '0',
                 'description' => '<p>Welkom in de Blackboard course voor SOPRJ8. Je bent automatisch geenrolled en toegevoegd aan je projectgroep zoals deze in de SOPRJ7 blackboard course zat. Mocht dit onjuist zijn, laat dit dan even aan mij (Rik Meijer) even weten.</p><p>Verder wensen wij je weer veel leerzaam plezier komende weken!</p>',
                 'date' => '2021-04-15 12:00:00',
-                "address" => '1111AA'
+                "address" => '1111AA',
+                "municipality_id" => '1'
             ],[
                 'published' => '1',
                 'name' => 'Project D',
                 'active' => '0',
                 'description' => '<p>Welkom in de Blackboard course voor SOPRJ8. Je bent automatisch geenrolled en toegevoegd aan je projectgroep zoals deze in de SOPRJ7 blackboard course zat. Mocht dit onjuist zijn, laat dit dan even aan mij (Rik Meijer) even weten.</p><p>Verder wensen wij je weer veel leerzaam plezier komende weken!</p>',
                 'date' => '2021-04-15 12:00:00',
-                "address" => '1111AA'
+                "address" => '1111AA',
+                "municipality_id" => '1'
             ],[
                 'published' => '1',
                 'name' => 'Project E',
                 'active' => '0',
                 'description' => '<p>Welkom in de Blackboard course voor SOPRJ8. Je bent automatisch geenrolled en toegevoegd aan je projectgroep zoals deze in de SOPRJ7 blackboard course zat. Mocht dit onjuist zijn, laat dit dan even aan mij (Rik Meijer) even weten.</p><p>Verder wensen wij je weer veel leerzaam plezier komende weken!</p>',
                 'date' => '2021-04-15 12:00:00',
-                "address" => '1111AA'
+                "address" => '1111AA',
+                "municipality_id" => '1'
+            ],[
+                'published' => '1',
+                'name' => 'Project AA',
+                'active' => '0',
+                'description' => '<p>Welkom in de Blackboard course voor SOPRJ8. Je bent automatisch geenrolled en toegevoegd aan je projectgroep zoals deze in de SOPRJ7 blackboard course zat. Mocht dit onjuist zijn, laat dit dan even aan mij (Rik Meijer) even weten.</p><p>Verder wensen wij je weer veel leerzaam plezier komende weken!</p>',
+                'date' => '2021-04-15 12:00:00',
+                "address" => '1111AA',
+                "municipality_id" => '2'
             ],
         );
 
@@ -62,6 +76,12 @@ class ProjectSeeder extends Seeder
                 'description' => $project["description"],
                 'date' => $project["date"],
                 "address" => $project["address"]
+            ]);
+
+            
+            $municipality = Municipality::find($project["municipality_id"]);
+            $model->municipalities()->attach($municipality, [
+                'position' => 1
             ]);
 
             ProjectSlug::create([
