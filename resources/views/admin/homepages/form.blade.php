31ae0497854080df7b58de995d068f04bdbd91e3
matsn0w <bartscholtus@gmail.com>
2021-05-10 18:44:43 +0200

    split sections into fieldsets

--- resources/views/admin/homepages/form.blade.php
+++ resources/views/admin/homepages/form.blade.php
@@ -12,8 +12,14 @@
         'label' => 'Banner',
         'maxlength' => 100
     ])
-
-    @formField('repeater', ['type' => 'homepage_link_items'])
-
-    @formField('repeater', ['type' => 'partner_items'])
 @stop
+
+@section('fieldsets')
+    <a17-fieldset title="Links" id="links" :open="true">
+        @formField('repeater', ['type' => 'homepage_link_items'])
+    </a17-fieldset>
+
+    <a17-fieldset title="Partners" id="partners" :open="true">
+        @formField('repeater', ['type' => 'partner_items'])
+    </a17-fieldset>
+@endsection
