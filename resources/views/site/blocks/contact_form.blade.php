d4757ac2230bb9defdc74c86d601d1bac387c8d8
xpetersen <ptrsnxndr@gmail.com>
2021-05-20 20:23:41 +0200

    Updated contact form to use bulma instead of tailwind

--- resources/views/site/blocks/contact_form.blade.php
+++ resources/views/site/blocks/contact_form.blade.php
@@ -14,49 +14,61 @@
         <h3 class="pl-4">{{ $block->input('title') }}</h3>
         <p class="pl-5">{{ $block->input('description') }}</p>
 
-        <form class="px-5" method="POST" action="{{ route('contact.sendMail') }}">
+        <form method="POST" action="{{ route('contact.sendMail') }}">
             @csrf
 
-            <div class="field input">
-                <input id="name" type="text" name="name" value="{{ old("name") }}" placeholder="Naam" required>
-                <label>Naam</label>
+            <div class="field">
+                <label class="label" for="name">Naam</label>
+                <div class="control has-icons-left">
+                    <input class="input is-rounded" id="name" type="text" name="name" value="{{ old("name") }}" placeholder="Naam" required>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                </div>
             </div>
 
             @error('name')
                 <div class="error">{{ $message }}</div>
             @enderror
 
-            <div class="field input">
-                <input id="email" type="email" name="email" value="{{ old("email") }}" placeholder="E-mailadres" required>
-                <label>E-mailadres</label>
+            <div class="field">
+                <label class="label" for="email">E-mailadres</label>
+                <div class="control has-icons-left">
+                    <input class="input is-rounded" id="email" type="email" name="email" value="{{ old("email") }}" placeholder="E-mailadres" required>
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                </span>
+                </div>
             </div>
+
             @error('email')
                 <div class="error">{{ $message }}</div>
             @enderror
 
-            <div class="field input">
-                <textarea class="contact-message" cols="30" rows="10" id="message" name="message" placeholder="Bericht" required>{{ old("message") }}</textarea>
-                <label class="pl-1">Bericht</label>
+            <div class="field">
+                <label class="label" for="message">Bericht</label>
+                <div class="control">
+                    <textarea class="textarea has-fixed-size" id="message" name="message" placeholder="Bericht" required>{{ old("message") }}</textarea>
+                </div>
             </div>
 
             @error('message')
                 <div class="error">{{ $message }}</div>
             @enderror
 
-            <div class="field input">
-                <select id="project" name="project">
-                    <option selected disabled>Selecteer een project</option>
-                    @foreach($projectList as $project)
-                        <option>{{$project->name}}</option>
-                    @endforeach
-                </select>
+            <div class="field">
+                <label class="label" for="project">(Optioneel) Selecteer een project</label>
+                <div class="control select is-rounded">
+                    <select id="project" name="project">
+                        <option selected disabled>Selecteer een project</option>
+                        @foreach($projectList as $project)
+                            <option>{{$project->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
 
-
-{{--            {!! NoCaptcha::renderJs() !!}--}}
-
-{{--            {!! NoCaptcha::display(['data-theme' => 'dark']) !!}--}}
-
+            {!! NoCaptcha::display() !!}
 
             @if ($errors->has('g-recaptcha-response'))
                 <span class="help-block">
@@ -64,15 +76,17 @@
                 </span>
             @endif
 
+            <div class="control">
+                <button class="button is-primary">Verstuur bericht</button>
+            </div>
+
             @if (session('success_message'))
-                <p class="contact-success-button">
-                    {{ session('success_message') }}
-                </p>
+                <p>{{ session('success_message') }}</p>
             @endif
         </form>
     </div>
 </div>
 
 @push('scripts')
-{{----}}
+    {!! NoCaptcha::renderJs() !!}
 @endpush
