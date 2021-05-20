d85b5c23a89c9c23aaaf70622956083070d06a5a
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-05-20 14:55:53 +0200

    Disables TailwindCSS in SCSS files and mixins

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+// const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -18,10 +18,10 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/css/app.scss', 'public/css')
     .sass('resources/css/newsItem.scss', 'public/css')
     .sass('resources/css/slideshow.scss', 'public/css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
-    })
+    // .options({
+    //     processCssUrls: false,
+    //     postCss: [tailwindcss('./tailwind.config.js')],
+    // })
     .copyDirectory('resources/img', 'public/img');
 
 mix.disableSuccessNotifications();
