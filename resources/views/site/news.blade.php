5b6335e2a762a6276df378705feb3e734864f094
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-05-22 21:52:20 +0200

    Added Clear search button and made search icon clickable

--- resources/views/site/news.blade.php
+++ resources/views/site/news.blade.php
@@ -7,45 +7,58 @@
         <div class="hero-body">
             <h1 class="is-size-1 has-text-centered has-text-weight-bold mt-2">Nieuws</h1>
             <h3 class="is-size-3-desktop is-size-4-tablet is-size-5-mobile has-text-centered mt-4 mb-5">Bekijk het laatste nieuws van het Tiny Restaurant en check wat er momenteel gaande is.</h3>
-            <div class="field search-input">
-                <label for="search" class="label sr-only">Zoek naar nieuwsberichten</label>
-                <div class="control has-icons-right">
-                    <form>
+            <form id="news-search-form">
+                <div class="search-input field has-addons">
+                    <p class="control is-expanded">
+                        <label for="search" class="sr-only">Zoeken in nieuwsberichten</label>
                         <input class="input" type="text" placeholder="Zoek naar nieuwsberichten..." name="search" id="search" @if(request()->has('search'))value="{{ request()->query('search') }}"@endif>
-                        <span class="icon is-small is-right">
+                    </p>
+                    <p class="control">
+                        <a class="button search-button" onclick="document.querySelector('#news-search-form').submit()">
                             <i class="fas fa-search"></i>
-                        </span>
-                    </form>
+                        </a>
+                    </p>
                 </div>
-            </div>
+                @if(request()->has('search'))
+                    <div class="clear-filter">
+                        <a href="{{ route('newsItems.show') }}">Verwijder "{{ request()->query('search') }}" filter</a>
+                    </div>
+                @endif
+            </form>
         </div>
     </section>
-    <section class="section section-timeline">
-        <div class="timeline">
-            <header class="timeline-header">
-                <span class="tag is-medium">Vandaag</span>
-            </header>
-            @foreach($newsItems as $newsItem)
-                <div class="timeline-item">
-                    <figure class="image timeline-image">
-                        @php($image = $newsItem->imageAsArray('cover', 'flexible'))
-                        <img src="{{ $image ? $image['src'] : asset('img/news-placeholder.png') }}" alt="{{ $image ? $image['alt'] : 'news placeholder' }}">
-                    </figure>
-                    <div class="timeline-marker"></div>
-                    <div class="timeline-content">
-                        <div class="timeline-content-wrapper">
-                            <small class="is-size-7 mb-2">{{ $newsItem->getCreatedTimeForView() }}</small>
-                            <p class="is-size-4 has-text-weight-bold mb-5">{{ $newsItem->title }}</p>
-                            <p>{{ $newsItem->summary }}</p>
+    @if(count($newsItems) > 0)
+        <section class="section section-timeline">
+            <div class="timeline">
+                <header class="timeline-header">
+                    <span class="tag is-medium">Vandaag</span>
+                </header>
+                @foreach($newsItems as $newsItem)
+                    <div class="timeline-item">
+                        <figure class="image timeline-image">
+                            @php($image = $newsItem->imageAsArray('cover', 'flexible'))
+                            <img src="{{ $image ? $image['src'] : asset('img/news-placeholder.png') }}" alt="{{ $image ? $image['alt'] : 'news placeholder' }}">
+                        </figure>
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                            <div class="timeline-content-wrapper">
+                                <small class="is-size-7 mb-2">{{ $newsItem->getCreatedTimeForView() }}</small>
+                                <p class="is-size-4 has-text-weight-bold mb-5">{{ $newsItem->title }}</p>
+                                <p>{{ $newsItem->summary }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-            <header class="timeline-header">
-                <span class="tag is-medium">Einde</span>
-            </header>
-        </div>
-    </section>
+                @endforeach
+                <header class="timeline-header">
+                    <span class="tag is-medium">Einde</span>
+                </header>
+            </div>
+        </section>
+    @else
+        <section class="section">
+            <h3 class="is-size-4 has-text-centered">Het hebben geen nieuwsberichten voor je kunnen vinden :(</h3>
+        </section>
+    @endif
 @endsection
 
 @section('old_content')
