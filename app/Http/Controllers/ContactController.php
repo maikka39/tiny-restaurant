d0dd5b34d962e0bd090ef14ac1c8da0eb9a81325
xpetersen <ptrsnxndr@gmail.com>
2021-05-25 15:42:26 +0200

    renamed MailController to ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\EmailFromUser;
+use App\Models\NewsItem;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,19 +12,25 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\SendEmailRequest;
 
-class MailController extends BaseController
+class ContactController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public function view()
+    {
+        return view('site.contact');
+    }
+
     public function sendMail(SendEmailRequest $request): \Illuminate\Http\RedirectResponse
     {
+        dd($request);
         $validated = $request->validated();
 
         if(is_null($request['project'])) {
             $request['project'] = "Geen project geselecteerd";
         }
 
-        Mail::queue(new EmailFromUser($validated['message'], $validated['name'], $validated['email'], $request['project']));
+        Mail::queue(new EmailFromUser($validated['message'], $validated['firstname'], $validated['lastname'], $validated['email'], $request['project']));
 
         $request->session()->flash('success_message', 'Email verzonden!');
 
