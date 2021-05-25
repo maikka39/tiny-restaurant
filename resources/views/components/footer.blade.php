9bbd5aa94acf0032ac4801f95035c899e19e73c5
Bram van den Boomen <b.vandenboomen@student.avans.nl>
2021-05-25 17:33:42 +0200

    The footer now implements the new design

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,23 +1,39 @@
-<footer class="footer">
-    <div class="container mx-auto px-6">
-        <div class="sm:flex sm:mt-8">
-            <div class="footer-wrapper sm:w-full sm:px-8 md:flex-row">
-                <div class="flex flex-col">
-                    <span class="footer-title">Onderwerpen</span>
-                    <span class="my-2"><a href="#" class="footer-text">Tiny Restaurant</a></span>
-                    <span class="my-2"><a href="#" class="footer-text">Voedseleducatie</a></span>
+<footer>
+    <div class="container">
+        <div class="columns is-vcentered">
+            <a class="column has-text-centered" href="">Nieuws</a>
+            <a class="column has-text-centered" href="">Projecten</a>
+            <a class="column has-text-centered" href="">Algemene voorwaarden</a>
+            <div class="column is-flex is-horizontal-center">
+                <figure class="image">
+                    <img src="{{ asset('img/logo.png') }}"/>
+                </figure>
+            </div>
+            <a class="column has-text-centered" href="">Privacy</a>
+            <a class="column has-text-centered" href="">Contact</a>
+            <a class="column has-text-centered" href="">Steun ons</a>
+        </div>
+        <hr>
+        <div>
+            <div class="pb-6 media-links">
+                <i class="fab fa-facebook fa-3x"></i>
+                <i class="fab fa-instagram fa-3x"></i>
+                <i class="fab fa-youtube fa-3x"></i>
+            </div>
+
+            <div class="columns .no-text-wrap">
+                <div class="column">
+                    <p class="has-text-weight-bold">Stichting MIEP</p>
+                    <p>Wijksestraat, 15</p>
+                    <p>06 - 20 46 65 55 (Krista Werker)</p>
                 </div>
-                <div class="flex flex-col">
-                    <span class="footer-title">Over ons</span>
-                    <span class="my-2"><a href="#" class="footer-text">Actueel</a></span>
-                    <span class="my-2"><a href="#" class="footer-text">Organistatie</a></span>
-                </div>
-                <div class="flex flex-col">
-                    <span class="footer-title">Contact</span>
-                    <span class="my-2"><a href="#" class="footer-text">E-mail ons</a></span>
-                    <span class="my-2"><a href="#" class="footer-text">Bel ons</a></span>
+                <div class="column">
+                    <a class="text-link" href=""> info@stichtingmiep.nl </a>
+                    <p>5256 BH Heusden</p>
+                    <p>06 - 24 62 96 76 (Jorick Cardon)</p>
                 </div>
             </div>
-        </div>
+            <p class="pt-4 has-text-centered">KvK:69331359, NL97 RABO 0321670485</p>
+            <p class="pt-6 pb-6 has-text-centered">TinyRestaurant. 2021. We houden van onze boeren!</p>
     </div>
 </footer>
