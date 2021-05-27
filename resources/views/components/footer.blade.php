555ae91da0c22cd4b899ae21ad61b730dd901c77
Maik de Kruif <maik@maik.dev>
2021-05-27 13:46:01 +0200

    Fix footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -45,7 +45,7 @@
                     <p>06 - 24 62 96 76 (Jorick Cardon)</p>
                 </div>
             </div>
-            <p class="pt-4 has-text-centered">KvK:69331359, NL97 RABO 0321670485</p>
-            <p class="pt-6 pb-6 has-text-centered">TinyRestaurant. 2021. We houden van onze boeren!</p>
+            <p class="pt-4 has-text-centered">KvK: 69331359, NL97 RABO 0321670485</p>
+            <p class="pt-6 has-text-centered">&copy; TinyRestaurant. 2021. We houden van onze boeren!</p>
     </div>
 </footer>
