c082e76e946d8a78839c080e3a09245c65d4ff62
matsn0w <bartscholtus@gmail.com>
2021-05-27 19:58:06 +0200

    made train a bit less obvious and suitable for mobile

--- resources/views/errors/base.blade.php
+++ resources/views/errors/base.blade.php
@@ -1,7 +1,7 @@
 @extends('site.layouts.base')
 
 @section('content')
-    <section class="section is-medium error-section">
+    <section class="section is-medium">
         <div class="container">
             <h1 class="title is-size-1">{{ $code ?? '' }}</h1>
             <h2 class="subtitle is-size-3">{{ $message ?? '' }}</h2>
@@ -10,9 +10,11 @@
                 <a class="button is-primary" href="/">Home</a>
             </div>
         </div>
+    </section>
 
+    <section class="section train">
         <marquee direction="right" scrollamount="10">
-            <img src="{{ asset('img/logo.png') }}" alt="Tiny Treintje">
+            <img src="{{ asset('img/train.png') }}" alt="Tiny Treintje">
         </marquee>
     </section>
 @endsection
