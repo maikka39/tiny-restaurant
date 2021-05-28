07aefbf962881b63ff6c3850d8f745a5201c12ae
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-05-28 17:21:46 +0200

    Added Bulma Carrousel

---
+++ resources/js/carrousel.js
@@ -0,0 +1,7 @@
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+const carousels = bulmaCarousel.attach('.carousel', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true
+});
--- resources/views/site/municipality.blade.php
+++ resources/views/site/municipality.blade.php
@@ -6,6 +6,10 @@
     <link rel="stylesheet" href="{{ asset('css/municipality.css') }}">
 @endpush
 
+@push('scripts')
+    <script type="text/javascript" src="{{ asset('js/carrousel.js') }}" defer></script>
+@endpush
+
 @section("content")
     <section class="section section-municipality-hero">
         <div class="municipality-text">
@@ -16,14 +20,37 @@
         <img class="municipality-image" src="{{ $image ? $image['src'] : asset('img/house-placeholder.png') }}" alt="{{ $image ? $image['alt'] : 'municipality placeholder' }}">
     </section>
     <section class="section section-farmers">
-        <div>
+        <div class="info">
             <h2>De boeren van {{ $municipality->title }}</h2>
             <p>Lees het verhaal van de boeren in gemeente {{ $municipality->title }}</p>
             <hr>
-            <iframe src="https://maps.google.com/maps?q={{ $item->title }}&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+            <iframe src="https://maps.google.com/maps?q={{ $municipality->title }}&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
         </div>
-        <div class="carousel">
-            //hier komen de boeren
+        <div class="farmers">
+            <div class="container">
+                <div class="carousel" id="municipality-carousel">
+                    @foreach($municipality->farmers as $farmer)
+                        <div class="column item-{{ $loop->iteration }}">
+                            <div class="card farmer-card">
+                                @php($image = $farmer->imageAsArray('farmer_profile', 'flexible'))
+                                @if ($image)
+                                    <div class="card-image">
+                                        <figure class="image is-3by2">
+                                            <img class="project-image" src="{{ $image['src'] }}" alt="{{ $image['alt'] }}">
+                                        </figure>
+                                    </div>
+                                @endif
+
+                                <div class="card-content">
+                                    <p class="title is-4">{{ $farmer->name }}</p>
+                                    <div class="content">{{ $farmer->summary }}</div>
+                                    <a class="button is-primary" href="{{ route('farmer.show', $farmer->slug) }}">Lees meer</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </section>
 @endsection
