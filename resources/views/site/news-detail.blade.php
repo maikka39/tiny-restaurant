81a900c0c30faa7722a87bba0c4604a6723afcdf
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-05-28 16:36:33 +0200

    Updated Layout

--- resources/views/site/news-detail.blade.php
+++ resources/views/site/news-detail.blade.php
@@ -13,15 +13,21 @@
             <h2 class="hero-news-title">{{ $news->title }}</h2>
             <p class="hero-news-summary">{{ $news->summary }}</p>
         </div>
-        @php($image = $news->imageAsArray('cover', 'flexible'))
-        <img src="{{ $image ? $image['src'] : asset('img/news-placeholder.png') }}" alt="{{ $image ? $image['alt'] : 'news placeholder' }}">
+        <h3 class="hero-images-title">Bekijk de uitgelichte fotos</h3>
+        <div class="images">
+            @foreach(array_slice($news->imagesAsArrays('cover', 'flexible'),0,3) as $image)
+                <figure class="image is-3by2">
+                    <img src="{{ $image ? $image['src'] : asset('img/news-placeholder.png') }}" alt="{{ $image ? $image['alt'] : 'news placeholder' }}">
+                </figure>
+            @endforeach
+        </div>
     </section>
     <section class="section section-news-description">
         <h2 class="description-title">Meer informatie</h2>
         <div class="description">{!! $news->description !!}</div>
     </section>
     <section class="section section-news-attachments">
-        <h2 class="attachments-title">Bekijk de bijgevoegde foto's en bestanden</h2>
+        <h2 class="attachments-title">Bekijk alle bijgevoegde foto's en bestanden</h2>
         <div class="attachments">
             @foreach($news->getTwillFilesWithName() as $file)
                 <div class="attachment">
