4829d91237ab10a2230016e023707bb11c8f1901
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-05-30 13:56:00 +0200

    Added Project Section to Municipality Page

--- resources/views/site/municipality.blade.php
+++ resources/views/site/municipality.blade.php
@@ -24,18 +24,20 @@
             <h2>De boeren van {{ $municipality->title }}</h2>
             <p>Lees het verhaal van de boeren in gemeente {{ $municipality->title }}</p>
             <hr>
-            <iframe src="https://maps.google.com/maps?q={{ $municipality->title }}&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+            <div class="map">
+                <iframe src="https://maps.google.com/maps?q={{ $municipality->title }}&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+            </div>
         </div>
         <div class="farmers">
             <div class="container">
                 <div class="carousel" id="municipality-carousel">
                     @foreach($municipality->farmers as $farmer)
                         <div class="column item-{{ $loop->iteration }}">
-                            <div class="card farmer-card">
+                            <div class="card farmer-card item-{{ $loop->iteration }}">
                                 @php($image = $farmer->imageAsArray('farmer_profile', 'flexible'))
                                 @if ($image)
                                     <div class="card-image">
-                                        <figure class="image is-3by2">
+                                        <figure class="image">
                                             <img class="project-image" src="{{ $image['src'] }}" alt="{{ $image['alt'] }}">
                                         </figure>
                                     </div>
@@ -48,8 +50,49 @@
                                 </div>
                             </div>
                         </div>
+                        <span></span>
                     @endforeach
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <div class="block">
+                <h2 class="title is-size-1 has-text-weight-normal">Aanstaande projecten</h2>
+                <p class="subtitle is-size-3 mt-5">Hier is een korte agenda om te zien wat de aankomende projecten van de gemeente {{ $municipality->title }}.</p>
+                <a href="{{ route('project.showAll', ['search' => $municipality->title]) }}" class="button is-primary">Bekijk alle projecten</a>
+            </div>
+
+            <div class="columns">
+                @forelse($municipality->projects as $project)
+
+                    <div class="column">
+                        <div class="card project-card">
+                            @php($image = $project->imageAsArray('project_image', 'flexible'))
+                            @if ($image)
+                                <div class="card-image">
+                                    <figure class="image">
+                                        <img class="project-image" src="{{ $image['src'] }}" alt="{{ $image['alt'] }}">
+                                    </figure>
+                                </div>
+                            @endif
+
+                            <div class="card-content">
+                                <p class="title is-4">{{ $project->name }}</p>
+                                <p class="subtitle is-6">{{ $project->created_at->format('d-m-Y') }}</p>
+
+                                <div class="content">{!! $project->description !!}</div>
+
+                                <a href="{{ route('project.show', $project->slug) }}">Lees meer</a>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <div class="column">
+                        <p>Er zijn geen projecten!</p>
+                    </div>
+                @endforelse
             </div>
         </div>
     </section>
