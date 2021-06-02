16c1134dc5b12fbd5f75f6a58506d02b0d8cf62e
Maik de Kruif <maik@maik.dev>
2021-06-03 01:07:09 +0200

    Seed partners

--- database/seeders/HomepageSeeder.php
+++ database/seeders/HomepageSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Homepage;
 use App\Models\HomepageLinkItem;
+use App\Models\HomepagePartnerItem;
 
 class HomepageSeeder extends MediaSeeder
 {
@@ -48,6 +49,39 @@ class HomepageSeeder extends MediaSeeder
             'position' => 3,
             'homepage_id' => 1
         ]);
+
+        $partners = array([
+            'name' => 'Laarakker Bio',
+            'description' => '<p>Laarakker BIO, onderdeel van de Laarakker bedrijven groep, richt zich volledig op de teelt, verwerking en in-/verkoop van een breed pakket biologische en biologisch dynamische groenten.</p>',
+        ], [
+            'name' => 'Rabobank',
+            'description' => '<p>Rabo Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ac nunc nec libero fringilla ornare. Aenean tortor ipsum, porttitor sed libero in, condimentum cursus arcu. In hac habitasse platea dictumst. Donec rutrum commodo tortor sit amet elementum. Phasellus tempus est a risus aliquam gravida. Morbi libero lectus, auctor id elit sollicitudin, dapibus mollis diam. Nam pellentesque suscipit nibh id efficitur.</p>',
+        ], [
+            'name' => 'Eigezwijns',
+            'description' => '<p>Eigezwijns Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ac nunc nec libero fringilla ornare. Aenean tortor ipsum, porttitor sed libero in, condimentum cursus arcu. In hac habitasse platea dictumst. Donec rutrum commodo tortor sit amet elementum. Phasellus tempus est a risus aliquam gravida. Morbi libero lectus, auctor id elit sollicitudin, dapibus mollis diam. Nam pellentesque suscipit nibh id efficitur.</p>',
+        ], [
+            'name' => 'EuroToques',
+            'description' => '<p>Eurotoques Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ac nunc nec libero fringilla ornare. Aenean tortor ipsum, porttitor sed libero in, condimentum cursus arcu. In hac habitasse platea dictumst. Donec rutrum commodo tortor sit amet elementum. Phasellus tempus est a risus aliquam gravida. Morbi libero lectus, auctor id elit sollicitudin, dapibus mollis diam. Nam pellentesque suscipit nibh id efficitur.</p>',
+        ], [
+            'name' => 'Regio Noordoost Brabant',
+            'description' => '<p>RNOB Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ac nunc nec libero fringilla ornare. Aenean tortor ipsum, porttitor sed libero in, condimentum cursus arcu. In hac habitasse platea dictumst. Donec rutrum commodo tortor sit amet elementum. Phasellus tempus est a risus aliquam gravida. Morbi libero lectus, auctor id elit sollicitudin, dapibus mollis diam. Nam pellentesque suscipit nibh id efficitur.</p>',
+        ]);
+
+        foreach ($partners as $partner) {
+            $model = HomepagePartnerItem::create([
+                'name' => $partner["name"],
+                'description' => $partner["description"],
+                'homepage_id' => 1,
+            ]);
+
+            $this->seed_media(
+                "App\Models\HomepagePartnerItem",
+                $model->id,
+                "image",
+                preg_replace('/[^a-z]/', "_", strtolower($model->name)).".png",
+                "Logo van ".$model->name,
+            );
+        }
 
         $this->seed_media(
             "App\Models\Homepage",
