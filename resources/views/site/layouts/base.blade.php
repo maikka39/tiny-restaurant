3e990a2e9a13785a8aa338a39fd5fd42ed138d73
Maik de Kruif <maik@maik.dev>
2021-06-08 21:46:49 +0200

    Add meta tags for icons

--- resources/views/site/layouts/base.blade.php
+++ resources/views/site/layouts/base.blade.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
+
+    <link rel="icon" href="{{ url("/favicon.ico") }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url("/apple-touch-icon.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url("/favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url("/favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ url("/site.webmanifest") }}">
+    <link rel="mask-icon" href="{{ url("/safari-pinned-tab.svg") }}" color="#eaffea">
+    <meta name="msapplication-TileColor" content="#00a300">
+    <meta name="theme-color" content="#ffffff">
 
     @stack('meta')
 
